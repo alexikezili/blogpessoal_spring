180e15fe2887fc76b7e3fca9b2267b30c7231bfb
Alex Ikezili <alex.ikezili@icloud.com>
2025-05-20 17:07:11 -0300

    :sparkles: Criação da Classe Tema e relacionamento com a Classe Postagem

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,11 +3,12 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("descricao") String titulo);
 	
 }
