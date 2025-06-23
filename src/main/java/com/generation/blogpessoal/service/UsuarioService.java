5b84c415bc0760d37b4805ff595fb6e7df3307dc
Alex Ikezili <alex.ikezili@icloud.com>
2025-06-23 11:06:18 -0300

    fix: Correção para deploy

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -34,6 +34,7 @@ public class UsuarioService {
 			return Optional.empty();
 
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		usuario.setId(null);
 
 		return Optional.of(usuarioRepository.save(usuario));
 	
